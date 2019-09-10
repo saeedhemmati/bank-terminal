f290523a09f320a5787e74589225b02c56221d2d
Saeed Hemmati <saeedhemmati93@yahoo.com>
2019-09-10 14:53:39 +0430

    Implement get token for saman

--- src/config/terminals.php
+++ src/config/terminals.php
@@ -10,5 +10,6 @@ return [
     */
     'saman' => [
         'merchantId' => '',
+        'redirectUrl' => '',
     ],
 ];
--- src/saman/Saman.php
+++ src/saman/Saman.php
@@ -4,5 +4,32 @@ namespace BankTerminal\Saman;
 
 class Saman
 {
-    
+    private function generateResNum(): string
+    {
+        return hash('sha256', time());
+    }
+
+    /**
+     * @param String $amount
+     */
+    public function getToken($amount = '')
+    {
+        $client = new \SoapClient('https://sep.shaparak.ir/Payments/InitPayment.asmx?WSDL');
+        $generatedResNum = $this->generateResNum();
+        $result = $client->RequestToken(
+            config('terminals.saman.merchantId'),
+            $generatedResNum,
+            $amount,
+            '0',
+            '0',
+            '0',
+            '0',
+            '0',
+            '0',
+            'ResNum1',
+            'ResNum2',
+            '0',
+            config('terminals.saman.redirectUrl'),
+        );
+    }
 }
